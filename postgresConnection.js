676df9fc4afa463f59d08e7321e068c66d742358
DaneenM <45468867+DaneenM@users.noreply.github.com>
2023-01-19 19:19:24 -0500

    update branch

---
+++ postgresConnection.js
@@ -0,0 +1,15 @@
+// const {Client} = require ('pg');
+// const client = new Client ({
+//     user: "admin",
+//     password: "none",
+//     host: "localhost",
+//     port: 5432,
+//     database: "pelican_database"
+// });
+
+// client.connect()
+// .then(() => console.log("Connected successfully"))
+// // .then(() => client.query("select * from mytable"))
+// // .then(results => console.table(results.columns))
+// .catch(err => console.log(err))
+// .finally(() => client.end())
